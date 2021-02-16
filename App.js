ae62c9eb2699356b329258ac825948efc12878e9
nikhil <nikhil.kp@iiflw.com>
2021-02-17 00:20:03 +0530

    final product

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect} from 'react';
+import { LogBox, StyleSheet, Text, View } from 'react-native';
+import RestaurantNavigator from './navigation/RestaurantNavigator';
+import {firebaseApp} from './utils/firebase';
+import * as firebase from 'firebase'
+import {encode,decode} from 'base-64'
 
+// if (!global.btoa) global.btoa = encode;
+// if (!global.atob) global.atob = decode;
+
+LogBox.ignoreLogs(["Setting a timer"])
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+      <RestaurantNavigator/>
+    );
+}
